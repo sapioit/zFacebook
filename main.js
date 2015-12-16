bcb1d1e3a18cffe9a00545ff2420e69631c41363
sapioit <sapioit@users.noreply.github.com>
2015-12-17 01:33:05 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+ var app = require('app');
+ var BrowserWindow = require('browser-window');
+ require('crash-reporter').start();
+
+ var mainWindow = null;
+
+ app.on('window-all-closed', function() {
+  if (process.platform != 'darwin')
+   app.quit();
+ });
+
+ app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+   width: 700,
+   height: 850,
+   resizable: true,
+   title: "zFacebook",
+   icon: "/zFacebook.icns",
+   icon: "/zFacebook.ico",
+   icon: "/zFacebook.png"
+  });
+  mainWindow.setOverlayIcon(
+   __dirname + "/zFacebook.png",
+   "Test?");
+  mainWindow.setTitle("zFacebook");
+  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.focus();
+  mainWindow.on('closed', function() {
+   mainWindow = null;
+  });
+  //mainWindow.toggleDevTools();
+ });
